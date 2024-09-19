13353d01cc88588bc841d33a32231b40136fef80
Mitsu325 <patricia.mashiba@gmail.com>
2024-09-19 20:49:10 -0300

    feat: add validators

--- backend/src/users/dto/create-user.dto.ts
+++ backend/src/users/dto/create-user.dto.ts
@@ -1,36 +1,44 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsEmail, IsOptional, IsString } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class CreateUserDto {
   @IsString()
+  @IsNotEmpty()
   @ApiProperty()
   name: string;
 
   @IsEmail()
+  @IsNotEmpty()
   @ApiProperty()
   email: string;
 
   @IsString()
+  @IsNotEmpty()
   @ApiProperty()
   cpf: string;
 
   @IsString()
+  @IsNotEmpty()
   @ApiProperty()
   rg: string;
 
   @IsString()
+  @IsNotEmpty()
   @ApiProperty()
   issuing_agency: string;
 
   @IsString()
+  @IsNotEmpty()
   @ApiProperty()
   postal_code: string;
 
   @IsString()
+  @IsNotEmpty()
   @ApiProperty()
   street: string;
 
   @IsString()
+  @IsNotEmpty()
   @ApiProperty()
   house_number: string;
 
@@ -40,18 +48,22 @@ export class CreateUserDto {
   complement?: string;
 
   @IsString()
+  @IsNotEmpty()
   @ApiProperty()
   neighborhood: string;
 
   @IsString()
+  @IsNotEmpty()
   @ApiProperty()
   city: string;
 
   @IsString()
+  @IsNotEmpty()
   @ApiProperty()
   state: string;
 
   @IsString()
+  @IsNotEmpty()
   @ApiProperty()
   password: string;
 }
