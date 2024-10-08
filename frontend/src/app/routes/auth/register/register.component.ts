b21409b238e5de5d59d7539007fcf30338d76eb3
Mitsu325 <patricia.mashiba@gmail.com>
2024-10-08 19:46:32 -0300

    feat: consultar API de cep

--- frontend/src/app/routes/auth/register/register.component.ts
+++ frontend/src/app/routes/auth/register/register.component.ts
@@ -8,6 +8,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { MatSelectModule } from '@angular/material/select';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-register',
@@ -57,7 +58,11 @@ export class RegisterComponent {
     'TO',
   ];
 
-  constructor(private fb: FormBuilder, private router: Router) {
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private http: HttpClient
+  ) {
     this.registerForm = this.fb.group({
       name: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
@@ -65,15 +70,62 @@ export class RegisterComponent {
       rg: ['', Validators.required],
       issuing_agency: ['', Validators.required],
       postal_code: ['', Validators.required],
-      street: ['', Validators.required],
-      house_number: ['', Validators.required],
-      complement: [''],
-      neighborhood: ['', Validators.required],
-      city: ['', Validators.required],
-      state: ['', Validators.required],
+      street: [{ value: '', disabled: true }, Validators.required],
+      house_number: [{ value: '', disabled: true }, Validators.required],
+      complement: [{ value: '', disabled: true }],
+      neighborhood: [{ value: '', disabled: true }, Validators.required],
+      city: [{ value: '', disabled: true }, Validators.required],
+      state: [{ value: '', disabled: true }, Validators.required],
       password: ['', [Validators.required, Validators.minLength(6)]],
       is_admin: [false],
     });
+  }
+
+  ngOnInit(): void {
+    this.registerForm.get('postal_code')?.valueChanges.subscribe((value) => {
+      if (value && value.length === 8) {
+        this.fetchAddressData(value);
+      } else {
+        this.disableAddressFields();
+      }
+    });
+  }
+
+  private fetchAddressData(postalCode: string): void {
+    this.enableAddressFields();
+
+    this.http.get(`https://opencep.com/v1/${postalCode}`).subscribe({
+      next: (response: any) => {
+        this.registerForm.patchValue({
+          street: response.logradouro,
+          neighborhood: response.bairro,
+          complement: response.complemento,
+          city: response.localidade,
+          state: response.uf,
+        });
+      },
+      error: (error) => {
+        console.error('Erro ao buscar dados do endereço:', error);
+      },
+    });
+  }
+
+  private enableAddressFields(): void {
+    this.registerForm.get('street')?.enable();
+    this.registerForm.get('house_number')?.enable();
+    this.registerForm.get('complement')?.enable();
+    this.registerForm.get('neighborhood')?.enable();
+    this.registerForm.get('city')?.enable();
+    this.registerForm.get('state')?.enable();
+  }
+
+  private disableAddressFields(): void {
+    this.registerForm.get('street')?.disable();
+    this.registerForm.get('house_number')?.disable();
+    this.registerForm.get('complement')?.disable();
+    this.registerForm.get('neighborhood')?.disable();
+    this.registerForm.get('city')?.disable();
+    this.registerForm.get('state')?.disable();
   }
 
   async onSubmit() {
